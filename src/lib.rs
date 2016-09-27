92e0588f6ef0549d4bc994808c7fb7f13a35a3aa
Ralph Giles <giles@mozilla.com>
2016-09-27 10:02:06 -0700

    Wrap foo in two separate crates and include them in the top-level.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-extern crate foo;
+extern crate wrap_a;
+extern crate wrap_b;
 
 pub fn result() -> u32 {
-    foo::bar() - 10
+    wrap_a::result() + wrap_b::result()
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(super::result(), 32);
+        assert_eq!(super::result(), 84);
     }
 }
