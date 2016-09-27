698de4e120eadaf077f6d14090caf1a34c6bcb06
Ralph Giles <giles@mozilla.com>
2016-09-27 09:56:09 -0700

    Call dependent foo crate.

--- foo/src/lib.rs
+++ foo/src/lib.rs
@@ -1,6 +1,11 @@
+pub fn bar() -> u32 {
+    42
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        assert_eq!(super::bar(), 42);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+extern crate foo;
+
+pub fn result() -> u32 {
+    foo::bar() - 10
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        assert_eq!(super::result(), 32);
     }
 }
